949cea266cfff3812f8f205ab479833a9405562b
Juan José González Alanís <juanjo.goal@gmail.com>
2022-06-15 15:31:26 +0200

    Added pr-code-sniff.yml workflow

---
+++ bin/phpcs-in-changes.sh
@@ -0,0 +1,14 @@
+#!/usr/scripts/env bash
+# Run PHP_CodeSniffer over the modified files for the specified commit range.
+
+COMMIT_RANGE="${1} ${2}"
+CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $COMMIT_RANGE | grep '\.php' | awk '{print}' ORS=' '`
+
+if [ "$CHANGED_FILES" != "" ]; then
+	echo "Changed files: $CHANGED_FILES"
+	echo "Running Code Sniffer."
+
+	./vendor/bin/phpcs --encoding=utf-8 -s -n -p --report-full --report-checkstyle=./phpcs-report.xml ${CHANGED_FILES}
+else
+	echo "No changes found. Skipping PHPCS run."
+fi
